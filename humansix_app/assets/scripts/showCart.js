112f5e0b380fadb0fb5b8cf249e88f525f51e12e
louis <louis.barre.info@gmail.com>
2020-12-27 20:17:05 +0100

    add authenticathor

---
+++ humansix_app/assets/scripts/showCart.js
@@ -0,0 +1,19 @@
+
+
+const showCart = () =>{
+    const showCartButtons = document.querySelectorAll('.show-cart-btn');
+
+    showCartButtons.addEventListener('click', ()=>{
+        //get id de la commande
+
+        //générer la fenetre
+         /*
+           - un entete
+           - la liste de la commande
+           - un bouton toggle
+          */
+
+    })
+}
+
+showCart();
